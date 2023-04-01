c0906940714a97dac268a9f65cfa792c802af602
Huang Yu <huangyu@Huangs-Mac-mini.attlocal.net>
2023-03-31 21:33:06 -0700

    :white_check_mark: added searchbar for homepage

--- where4dinner/components/HeaderTabs.js
+++ where4dinner/components/HeaderTabs.js
@@ -26,7 +26,7 @@ export default function HeaderTabs() {
 const HeaderButton = (props) => (
     <TouchableOpacity
         style={{
-            backgroundColor: props.activeTab == props.Text ? "#FF6961":"white",
+            backgroundColor: props.activeTab == props.Text ? "#63C5DA":"white",
             paddingVertical: 6,
             paddingHorizontal: 16,
             borderRadius: 40
@@ -35,7 +35,7 @@ const HeaderButton = (props) => (
     >
         <Text
             style={{
-            color: props.activeTab == props.Text ? "white":"#FF6961",
+            color: props.activeTab == props.Text ? "white":"#63C5DA",
             fontSize: 15,
             fontWeight: "900"
         }}
--- where4dinner/screens/Home.js
+++ where4dinner/screens/Home.js
@@ -2,12 +2,14 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   return (
-    <SafeAreaView style={{ backgroundColor: "#FF6961", flex:1 }}>
+    <SafeAreaView style={{ backgroundColor: "#82EEFD", flex:1 }}>
         <View style={{ backgroundColor: "white", padding: 10 }}>
             <HeaderTabs />
+            <SearchBar />
         </View>
     </SafeAreaView>
     
