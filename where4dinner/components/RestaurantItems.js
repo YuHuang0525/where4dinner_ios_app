2a4ad08ec1acd3dd25acb7e92e4bfaacb9f68654
YuHuang0525 <yu.huang2018@gmail.com>
2023-04-03 16:49:53 -0700

    :sparkles: added yelp api and processed data from yelp api for a specific location

---
+++ where4dinner/components/RestaurantItems.js
@@ -0,0 +1,80 @@
+import { View, Text, Image, TouchableOpacity} from 'react-native'
+import React from 'react'
+
+export const localRestaurants = [
+    {
+        name: "Noble Chef",
+        image_url: "https://media-cdn.tripadvisor.com/media/photo-s/10/dd/41/93/noble-chef.jpg",
+        categories: ["Chinese"],
+        price: "$$",
+        reviews: 1244,
+        rating: 5.0
+    },
+    {
+        name: "Eastern Dynasty",
+        image_url: "https://media-cdn.tripadvisor.com/media/photo-s/10/dd/41/93/noble-chef.jpg",
+        categories: ["Chinese"],
+        price: "$$",
+        reviews: 1244,
+        rating: 4.5
+    },
+    {
+        name: "Noble Chef",
+        image_url: "https://media-cdn.tripadvisor.com/media/photo-s/10/dd/41/93/noble-chef.jpg",
+        categories: ["Chinese"],
+        price: "$$",
+        reviews: 1244,
+        rating: 5.0
+    }
+] 
+
+
+export default function RestaurantItems(props) {
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 10 }}>
+        {props.restaurantData.map((restaurant, index) => (
+            <View key={index} style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}>
+                <RestaurantImage image={restaurant.image_url}/>
+                <RestaurantInfo name={restaurant.name} rating={restaurant.rating}/>
+            </View>   
+        ))}
+    </TouchableOpacity>
+  )
+}
+
+const RestaurantImage = (props) => (
+    <Image
+        source={{
+            uri: props.image
+        }}
+        style={{ width: "100%", height: 200 }}
+    />
+)
+
+const RestaurantInfo = (props) => (
+    <View 
+        style={{ 
+            flexDirection: "row", 
+            justifyContent: "space-between", 
+            alignItems: "center", 
+            marginTop: 10}}>
+        
+        <View> 
+            <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
+            <Text style={{ fontSize: 13, color: "gray" }}>30-45 min</Text>
+        </View>
+
+        <View
+            style={{
+            backgroundColor: "#82EEFD",
+            height: 30,
+            width: 30,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 15
+            }}
+        >
+            <Text>{props.rating}</Text>
+        </View>
+    </View>
+)
--- where4dinner/screens/Home.js
+++ where4dinner/screens/Home.js
@@ -1,12 +1,36 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
-import RestaurantItem from '../components/RestaurantItem';
+import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
+
+const YELP_API_KEY = "pJgoxwFOG_CuAoI5P-J-20p9EAtY5QJLjBnBUXe5bCt3uonHvDFSjQZRMDVoLoTTCuZIH8IAvzZq2BS5TKYKMa-EGTYV42z4bOJxCjBopXN1Z2l6REjQEtcgol8rZHYx"
+
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = useState(localRestaurants)
+  
+  const getRestaurntsFromYelp = () => {
+    const yelpUrl = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=San+Diego%2C+CA"
+  
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`    
+      },
+    }
+
+    return fetch(yelpUrl, apiOptions).then((res) => res.json()).then(json => setRestaurantData(json.businesses))
+
+  }
+
+  useEffect(() => {
+    getRestaurntsFromYelp();
+  }, [])
+  
+
   return (
     <SafeAreaView style={{ backgroundColor: "#82EEFD", flex:1 }}>
         <View style={{ backgroundColor: "white", padding: 10 }}>
@@ -15,9 +39,7 @@ export default function Home() {
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
             <Categories />
-            <RestaurantItem />
-            <RestaurantItem />
-            <RestaurantItem />
+            <RestaurantItems restaurantData={restaurantData} />
         </ScrollView>
     </SafeAreaView>
     
